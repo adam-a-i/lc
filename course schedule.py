4085e39357a5c02f74316f7781d7407acef6e80b
adam <143326543+adam-a-i@users.noreply.github.com>
2025-09-11 16:44:02 +0400

    Implement course scheduling solution using DFS

---
+++ course schedule.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+
+        graph = {i: [] for i in range(numCourses)}
+        for course, pre in prerequisites:
+            graph[course].append(pre)
+
+        def dfs(node):
+            if node in visiting:
+                return False
+            if graph[node] == []:
+                return True
+            visiting.add(node)
+            for pre in graph[node]:
+                if not dfs(pre):
+                    return False
+            visiting.remove(node)
+            return True
+        visiting = set()
+
+        for course in graph:
+            if not dfs(course):
+                return False
+
+        return True
+
+
+        
+            
+
+        
